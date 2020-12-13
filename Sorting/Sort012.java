3bdc736bfddaa97ab95d9a80ee17cd344c7f5cbe
Uttam Mittal <uttam29mittal@gmail.com>
2020-12-14 00:13:49 +0530

    Add files via upload

---
+++ Sorting/Sort012.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class Sort012 {
+    public static void sort012(int[] arr){
+        int i = 0, j = 0, k = arr.length - 1;
+        while (i <= k) {
+            if (arr[i] > 1) {
+                swap(arr, i, k);
+                k --;
+            }
+            else if(arr[i] < 1) {
+                swap(arr, i, j);
+                i ++;
+                j ++;
+            }
+            else {
+                i ++;
+            }
+        }
+
+    }
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping index " + i + " and index " + j);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void print(int[] arr){
+        for(int i = 0 ; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        sort012(arr);
+        print(arr);
+    }
+}
